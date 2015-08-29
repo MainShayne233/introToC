e17e7fe444ba2c4a04a3d22a22e40584792b1f09
Shayne Tremblay <shaynetremblay@hotmail.com>
2015-08-28 23:23:12 -0400

    bonus1

---
+++ submissions/bonus1.c
@@ -0,0 +1,13 @@
+// Shayne Tremblay
+// 8-24-2015
+// Bonus Program 1
+
+#include <stdio.h>
+
+int main() {
+    
+    printf("Hello World! How great it is to be compiled!\n");
+    
+    return 0;
+    
+}
