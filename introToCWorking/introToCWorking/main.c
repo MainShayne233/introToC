039be4330318086e0d4f041801f0d4c7355778cc
MainShayne233 <shaynetremblay@hotmail.com>
2015-10-25 23:34:17 -0400

    assignment 6

--- introToCWorking/introToCWorking/main.c
+++ introToCWorking/introToCWorking/main.c
@@ -4,7 +4,7 @@
 
 int main(){
     
-    int riders_amt, rider, item, item_bool, lose_item, i, flag = 1;
+    int riders_amt, item, item_bool, lose_item, i, total, seed, tossout, rider = 1, flag = 1;
     
     int inventory[6][6] = {0};
     
@@ -14,69 +14,83 @@ int main(){
     
     scanf("%d",&riders_amt);
     
+    seed = time(NULL);
+    
+    srand(seed);
+    
     while (flag == 1){
         
         for (rider = 1; rider <= riders_amt; rider++){
-            printf("Rider %d, it is your turn. Enter 0 to look for an item\n\n");
-            scanf("%d");
+            printf("\nRider %d, it is your turn. Enter 0 to look for an item.\n", rider);
+            scanf("%d",&tossout);
             
-            srand(time(0));
-            
-            item = rand() % 7;
+            item = ((rand()/157) % 7);
             
             if (item != 6){
-                printf("Rider %d, you have found %s.\n",rider,item_names[item]);
+                printf("\nRider %d, you have found %s.\n",rider,item_names[item]);
                 if (item != 1 && item != 3){
                     if (inventory[rider-1][item] == 0){
                         inventory[rider-1][item] = 1;
-                        printf("You now have %s\n",item_names[item]);
+                        printf("You now have %s!\n",item_names[item]);
                     }
                     else{
-                        printf("You already have that item.\n");
+                        printf("\nYou already have that item.\n");
                     }
                 }
                 else{
                     if (inventory[rider-1][item] == 0){
                         if (inventory[rider-1][item-1] == 0){
-                            printf("Sorry, you can't obtain %s\n",item_names[item]);
+                            printf("\nSorry, you can't obtain %s.\n",item_names[item]);
                         }
                         else{
                             inventory[rider-1][item] = 1;
-                            printf("You now have %s\n",item_names[item]);
+                            printf("\nYou now have %s\n",item_names[item]);
                         }
                     }
                     else{
-                        printf("You already have that item.\n");
+                        printf("\nYou already have that item.\n");
                     }
                 }
             }
             else{
-                printf("You've been caught!\n");
+                printf("\nYou've been caught!\n");
                 item_bool = 0;
                 for (i = 0; i < 6; i++){
-                    if (inventory[rider][i] == 1)
+                    if (inventory[rider-1][i] == 1)
                         item_bool = 1;
                 }
                 if (item_bool == 1){
-                    printf("Which piece would you like to lose?\n");
+                    printf("\nWhich piece would you like to lose?\n");
                     for (i = 0; i < 6; i++){
-                        if (inventory[rider][i] == 1)
-                            printf("%d. %s",i,item_names[i]);
+                        if (inventory[rider-1][i] == 1)
+                            if (!((i == 0 && inventory[rider-1][1] == 1) || (i == 2 && inventory[rider-1][3] == 1)))
+                                printf("%d. %s\n",i,item_names[i]);
                     }
-                    scanf("%d",lose_item);
+                    scanf("%d",&lose_item);
                     
-                    inventory[rider][lose_item] = 0;
+                    inventory[rider-1][lose_item] = 0;
                     
-                    printf("You have selected the %s to discard.\n", item_names[lose_item]);
+                    printf("\nYou have selected the %s to discard.\n", item_names[lose_item]);
                 }
                 else{
-                    printf("There is no equipment for you to lose. Lucky you, sort of.\n");
+                    printf("\nThere is no equipment for you to lose. Lucky you, sort of.\n");
                 }
-                
+            }
+            printf("\nRider %d, here is what you currently have:\n", rider);
+            total = 0;
+            for (i = 0; i < 6; i++){
+                if (inventory[rider-1][i] == 1){
+                    printf("%s\n",item_names[i]);
+                    total += 1;
+                }
+            }
+            if (total == 6){
+                flag = 0;
+                break;
             }
         }
-        
-        
     }
+    printf("\nCongratulations Rider %d, you are the first dragon rider!\n\n",rider);
     
+    return 0;
 }
